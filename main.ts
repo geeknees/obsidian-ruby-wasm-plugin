3adcb3b70ab6ceff68ae25d72f5e3c6074c32c05
Masumi Kawasaki <geeknees@gmail.com>
2024-04-28 15:13:16 +0900

    Update versions.json, manifest.json, package.json, tsconfig.json, and esbuild.config.mjs

--- main.ts
+++ main.ts
@@ -1,89 +1,123 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+
+import { DefaultRubyVM } from "@ruby/wasm-wasi/dist/browser";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface RubyWasmPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: RubyWasmPluginSettings = {
+	mySetting: "default",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class RubyWasmPlugin extends Plugin {
+	settings: RubyWasmPluginSettings;
+
+	// Function to check if inside code block
+	isInCodeBlock = (editor: Editor, line: number) => {
+		const totalLines = editor.lineCount();
+		let inCodeBlock = true;
+		for (let i = line; i >= 0; i--) {
+			const lineText = editor.getLine(i).trim();
+			if (lineText.startsWith("```")) {
+				inCodeBlock = !inCodeBlock;
+			}
+		}
+		for (let i = line + 1; i < totalLines; i++) {
+			const lineText = editor.getLine(i).trim();
+			if (lineText.startsWith("```")) {
+				inCodeBlock = !inCodeBlock;
+				break;
+			}
+		}
+		return inCodeBlock;
+	};
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		const response = await fetch(
+			"https://cdn.jsdelivr.net/npm/@ruby/3.3-wasm-wasi@2.5.1/dist/ruby+stdlib.wasm"
+		);
+		const module = await WebAssembly.compileStreaming(response);
+		const { vm } = await DefaultRubyVM(module);
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "run-in-modal-command",
+			name: "Run in Modal",
+			editorCallback: (editor: Editor) => {
+				const code = editor.getSelection();
+				let result;
+				try {
+					result = vm.eval(code).toString();
+				} catch (e) {
+					result = e.toString();
+				}
+				// console.log(code);
+				new CodeModal(this.app, code, result).open();
+			},
 		});
+
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "run-in-editor-command",
+			name: "Run in Editor",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const code = editor.getSelection();
+				let result;
+				try {
+					result = vm.eval(code).toString();
+				} catch (e) {
+					result = e.toString();
 				}
-			}
+				// console.log(code);
+				const cursorLine = editor.getCursor().line;
+				const insideCode = this.isInCodeBlock(editor, cursorLine);
+
+				if (insideCode) {
+					editor.replaceSelection(`${code}\n# => ${result}`);
+				} else {
+					editor.replaceSelection(
+						`${code}\n\`\`\`\n${result}\n\`\`\``
+					);
+				}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new RubyWasmSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+		// 	console.log("click", evt);
+		// });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerInterval(
+		// 	window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		// );
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,44 +125,69 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class CodeModal extends Modal {
+	code: string;
+	result: string;
+	constructor(app: App, code: string, result: string) {
 		super(app);
+		this.code = code;
+		this.result = result;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	async onOpen() {
+		const { contentEl } = this;
+
+		// contentEl.createEl("h1", { text: "Code" });
+		const codeBlock = contentEl.createEl("pre", {
+			cls: "language-ruby",
+		});
+		const codeElement = codeBlock.createEl("code", {
+			cls: "language-ruby",
+		});
+		codeElement.textContent = this.code || "code";
+
+		const resultElement = contentEl.createEl("div", {
+			cls: "ruby-output",
+			attr: { style: "white-space: pre-wrap;" },
+		});
+		resultElement.textContent = this.result || "result";
+
+		const closeButton = contentEl.createEl("button", { text: "Close" });
+		closeButton.style.cssText = "float: right;";
+		closeButton.addEventListener("click", () => {
+			this.close();
+		});
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class RubyWasmSettingTab extends PluginSettingTab {
+	plugin: RubyWasmPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: RubyWasmPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		new Setting(containerEl).setName("ruby.wasm");
+		// .setDesc("It's a secret");
+		// .addText((text) =>
+		// 	text
+		// 		.setPlaceholder("Enter your secret")
+		// 		.setValue(this.plugin.settings.mySetting)
+		// 		.onChange(async (value) => {
+		// 			this.plugin.settings.mySetting = value;
+		// 			await this.plugin.saveSettings();
+		// 		})
+		// );
 	}
 }
